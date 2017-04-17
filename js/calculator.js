1afcfea855f6b1d9b5f1736f69b3f0f1ece2ddff
Jaime Moncada <jaimemoncada13@gmail.com>
2017-04-17 15:56:37 -0500

    added a new function in js and styled up the calc

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+/**
+ * Created by
+ * jaimemoncada on 4/17/17.
+ */
+
+(function () {
+    "use strict";
+
+var screenOutPut = document.getElementById('screen1');
+var numberButton = document.getElementsByClassName('buttons2')[0];
+
+
+function outPutScreen() {
+    screenOutPut.innerHTML = (1);
+}
+
+    numberButton.addEventListener("click", outPutScreen);
+
+
+})();
